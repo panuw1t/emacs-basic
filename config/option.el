31d30e6f3c34457c4e693425dfc98d343873e81c
panuvit sreudomdetsakul <panuvit@asger.co>
2025-09-04 20:17:28 +0700

    config compilation mode

--- config/option.el
+++ config/option.el
@@ -51,3 +51,48 @@
 (add-hook 'text-mode-hook #'display-line-numbers-mode)
 (add-hook 'after-save-hook
           #'executable-make-buffer-file-executable-if-script-p)
+
+
+;; compilation
+(setopt compilation-always-kill t)
+(setopt compilation-environment
+        (list (concat "PATH=" (getenv "HOME") "/.bun/bin:" (getenv "PATH"))))
+
+(add-hook 'compilation-mode-hook
+          (lambda ()
+            (setq truncate-lines t)))
+
+(with-eval-after-load 'compile
+  ;; set cursor to follow compilation output
+  (setq compilation-scroll-output t)
+
+	(add-to-list 'compilation-error-regexp-alist 'gradle-kotlin)
+	(add-to-list 'compilation-error-regexp-alist-alist
+							 '(gradle-kotlin
+								 "^e: file://\\([^:]+\\):\\([0-9]+\\):\\([0-9]+\\)"
+								 1 2 3)))
+
+;; color for compile buffer ?
+(require 'ansi-color)
+(defun colorize-compilation-buffer ()
+  (let ((inhibit-read-only t))
+    (ansi-color-apply-on-region (point-min) (point-max))))
+(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
+
+;; project.el
+(with-eval-after-load 'project
+	(add-to-list 'project-switch-commands '(project-compile "compile")))
+
+;; visual line mode
+(with-eval-after-load 'simple
+  (define-key visual-line-mode-map [remap move-beginning-of-line] #'move-beginning-of-line)
+  (define-key visual-line-mode-map [remap move-end-of-line] #'move-end-of-line)
+  (define-key visual-line-mode-map [remap kill-line] #'kill-line))
+
+(global-visual-line-mode 1)
+
+;; org mode
+(with-eval-after-load 'org
+	(global-set-key (kbd "C-c l") #'org-store-link)
+	(global-set-key (kbd "C-c a") #'org-agenda)
+	(global-set-key (kbd "C-c c") #'org-capture))
